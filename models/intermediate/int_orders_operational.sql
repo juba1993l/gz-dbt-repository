07445c8b41cfb9f271457082b110a299f933f80e
la j <larbi.juba@outlook.com>
2024-07-24 11:26:28 +0000

    xgfh

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,9 +1,16 @@
 select
-m.orders_id,
-m.date_date,
-ROUND(SUM(cast(m.margin as decimal) + cast(ship.shipping_fee as decimal) - cast(ship.logcost as decimal) - cast(ship.ship_cost as decimal) ),2) as Operational_margin
-from {{ref("int_orders_margin")}} as m
-left join {{ref("raw_gz_ship")}} as ship
-using (orders_id)
+    m.orders_id,
+    m.date_date,
+    round(
+        sum(
+            cast(m.margin as decimal)
+            + cast(ship.shipping_fee as decimal)
+            - cast(ship.logcost as decimal)
+            - cast(ship.ship_cost as decimal)
+        ),
+        2
+    ) as operational_margin
+from {{ ref("int_orders_margin") }} as m
+left join {{ ref("raw_gz_ship") }} as ship using (orders_id)
 group by m.orders_id, m.date_date
 order by m.orders_id desc
