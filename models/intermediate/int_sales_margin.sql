93c966d11da4598f65c3230be51204894ab15cb4
la j <larbi.juba@outlook.com>
2024-07-24 08:37:37 +0000

    margin

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,12 @@
+SELECT 
+    products_id, 
+    date_date, 
+    orders_id,
+    revenue, 
+    quantity, 
+    CAST(purchase_price AS FLOAT64), 
+    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+FROM {{ref("raw_gz_sales")}} s
+LEFT JOIN {{ref("raw_gz_product")}} p 
+    USING (products_id)
