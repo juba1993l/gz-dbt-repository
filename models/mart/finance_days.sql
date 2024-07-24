312965705ba4f4fc957cbf1c0082a2ec9ff3c3da
la j <larbi.juba@outlook.com>
2024-07-24 12:18:40 +0000

    cjgk

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,11 +1,15 @@
 select
-op.date_date,
-count (op.orders_id) as nb_operations,
-round(Sum(op.revenue)) as revenue,
-round((sum(op.revenue)/count (op.orders_id) ),1) as average_basket,
-round(sum(op.margin)) as margin,
-round(sum(op.operational_margin)) as operational_margin
-
-from {{ ref("int_orders_operational")}} as op
+    op.date_date,
+    count(op.orders_id) as nb_operations,
+    round(sum(cast(op.revenue as numeric))) as revenue,
+    round((sum(cast(op.revenue as numeric)) / count(op.orders_id)), 1) as average_basket,
+    round(sum(cast(op.margin as numeric))) as margin,
+    round(sum(cast(op.operational_margin as numeric))) as operational_margin,
+    round(sum(cast(op.purchase_cost as numeric))) as purchase_cost,
+    round(sum(cast(op.shipping_fee as numeric))) as shipping_fee,
+    round(sum(cast(op.logcost as numeric))) as logcost,
+    round(sum(cast(op.ship_cost as numeric))) as ship_cost,
+    round(sum(cast(op.quantity as numeric))) as quantity
+from {{ ref("int_orders_operational") }} as op
 group by op.date_date
 order by op.date_date desc
