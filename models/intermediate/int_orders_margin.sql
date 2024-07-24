7005ec8c3dc817d10b14764d1d390a574a6d9285
la j <larbi.juba@outlook.com>
2024-07-24 09:35:14 +0000

    mm

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,22 @@
+/*
 select
-
-cast (date_date as timestamp) as date_date,
-sum(revenue) as revenue,
-sum(quantity) as quantity,
-sum(purchase_cost) as purchase_cost,
-sum(margin) as margin
+orders_id,
+date_date,
+round(sum(revenue),2) as revenue,
+round(sum(quantity),2) as quantity,
+round(sum(purchase_cost),2) as purchase_cost,
+round(sum(margin),2) as margin
 from {{ref("int_sales_margin")}} as margin
-group by orders_id
+group by orders_id, date_date
 order by orders_id desc
+*/
+SELECT
+  orders_id,
+  date_date,
+  ROUND(SUM(revenue),2) as revenue,
+  ROUND(SUM(quantity),2) as quantity,
+  ROUND(SUM(purchase_cost),2) as purchase_cost,
+  ROUND(SUM(margin),2) as margin
+FROM {{ ref("int_sales_margin") }}
+GROUP BY orders_id,date_date
+ORDER BY orders_id DESC
